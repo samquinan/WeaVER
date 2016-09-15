a8207e3ec29eb184f90bfe0c64c9f632d1e65b1b
Sam Quinan <samquinan@gmail.com>
2016-09-15 15:17:20 -0600

    fixed routine broken by an errant delete during commenting pass

--- processing/programs/src/gen_wind_speed.cpp
+++ processing/programs/src/gen_wind_speed.cpp
@@ -86,6 +86,7 @@ int main(int argc, char* argv[])
 				for (int i=0; i < s; i++){
 					// calculate speed and convert units if appropriate
 					double tmp = (sqrt(pow(dU[i], 2.0) + pow(dV[i], 2.0))) * conversion_constant;
+					speed.push_back(tmp);
 				}
 				
 				// write wind speed	
